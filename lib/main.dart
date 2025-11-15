d9fd124c96846e4815b9b5b85590ad51cee5af9a
Lynx20wz <maks20wz@gmail.com>
2025-11-15 14:10:27 +0300

    Improve schedule print

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart' show DateFormat;
 import 'package:wear_plus/wear_plus.dart';
 
 import 'api.dart';
@@ -31,52 +31,48 @@ class _StartPageState extends State<StartPage> {
   Schedule? schedule;
 
   @override
-  void initState() {
-    super.initState();
-    _getSchedule();
-  }
-
-  void _getSchedule() async {
-    schedule = Schedule.fromJson(await getSchedule());
-    setState(() {});
-  }
-
-  @override
   Widget build(BuildContext context) => Scaffold(
-    body: Center(
-      child: WatchShape(
-        builder: (context, watchShape, _) => Container(
-          // decoration: BoxDecoration(
-          //   shape: watchShape == WearShape.round
-          //       ? BoxShape.circle
-          //       : BoxShape.rectangle,
-          // ),
-          padding: const EdgeInsets.only(top: 8, right: 40, left: 40),
-          child: Column(
-            children: [
-              Text(
-                DateFormat('HH:mm').format(DateTime.now()),
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Expanded(
-                child: ListView.separated(
-                  shrinkWrap: true,
-                  itemCount: schedule?.days[currentWeekday]?.length ?? 1,
-                  separatorBuilder: (context, index) =>
-                      const SizedBox(height: 4),
-                  itemBuilder: (context, index) {
-                    final lessons = schedule?.days[currentWeekday];
-                    return lessons == null
-                        ? const CircularProgressIndicator()
-                        : LessonWidget(lessons[index]);
-                  },
-                ),
-              ),
-            ],
-          ),
+    body: WatchShape(
+      builder: (context, watchShape, _) => Padding(
+        padding: const EdgeInsets.only(right: 40, left: 40),
+        child: FutureBuilder(
+          future: getSchedule(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState != ConnectionState.done) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            final schedule = Schedule.fromJson(snapshot.data!);
+            final lessons = schedule.days[currentWeekday] ?? [];
+
+            if (lessons.isEmpty) {
+              return Column(
+                children: [
+                  WatchWidget(),
+                  const Expanded(
+                    child: Center(
+                      child: Text(
+                        'No lessons today',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            }
+
+            return ListView.separated(
+              padding: const EdgeInsets.only(bottom: 30),
+              itemCount: lessons.length + 1,
+              separatorBuilder: (context, index) => const SizedBox(height: 4),
+              itemBuilder: (context, index) => index == 0
+                  ? Center(child: WatchWidget())
+                  : LessonWidget(lessons[index - 1]),
+            );
+          },
         ),
       ),
     ),
@@ -97,7 +93,7 @@ class LessonWidget extends StatelessWidget {
         color: theme.colorScheme.secondaryContainer,
       ),
       child: Container(
-        margin: const EdgeInsets.all(4),
+        margin: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -115,6 +111,20 @@ class LessonWidget extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class WatchWidget extends StatelessWidget {
+  const WatchWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        DateFormat('HH:mm').format(DateTime.now()),
+        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
       ),
     );
   }
