90991fd7dbd519702bc96474733b77209166e700
Lynx20wz <maks20wz@gmail.com>
2025-11-18 20:52:02 +0300

    Split test

--- test/schedule_test.dart
+++ test/schedule_test.dart
@@ -1,26 +1,7 @@
-import 'package:schedule/classes/lesson.dart';
 import 'package:schedule/classes/schedule.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Lesson', () {
-    test('should create Lesson from valid JSON with timezone', () {
-      const json = {
-        'subject_name': 'Math',
-        'room_number': '101',
-        'start_at': '2024-01-01T09:00:00+03:00',
-        'finish_at': '2024-01-01T12:10:30+03:00',
-      };
-
-      final lesson = Lesson.fromMap(json);
-
-      expect(lesson.name, 'Math');
-      expect(lesson.room, '101');
-      expect(lesson.startAt, DateTime.utc(2024, 1, 1, 6, 0));
-      expect(lesson.finishAt, DateTime.utc(2024, 1, 1, 9, 10, 30));
-    });
-  });
-
   group('Schedule', () {
     test('should create Schedule with lessons with timezone', () {
       const json = {
@@ -57,7 +38,7 @@ void main() {
       ); // UTC
     });
 
-    test('days test', () {
+    test('days method test', () {
       const json = {
         'total_count': 2,
         'response': [
@@ -78,7 +59,7 @@ void main() {
 
       final schedule = Schedule.fromMap(json);
 
-      expect(schedule.days, {
+      expect(schedule.lessonsByDays, {
         'monday': [schedule.lessons[0]],
         'tuesday': [schedule.lessons[1]],
         'wednesday': [],
